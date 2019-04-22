dedf4d9d192dfc7bbebdcc94388a2057f3292e5a
manhht97 <manhht@talent.ominext.com>
2019-04-22 14:28:08 +0700

    'update'

--- src/stories/screens/Promotion/index.tsx
+++ src/stories/screens/Promotion/index.tsx
@@ -2,10 +2,7 @@
 import React, { Component } from 'react';
 import { Dimensions, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 
-import vcs from '../../../../assets/vcs.png'
-import sun from '../../../../assets/sun.png'
-import nhac from '../../../../assets/nhac.png'
-import saigon from '../../../../assets/saigon.png'
+import sale from '../../../../assets/sale.png'
 const deviceHeight = Dimensions.get("window").height;
 const deviceWidth = Dimensions.get("window").width;
 
@@ -20,12 +17,13 @@ export default class Promotion extends Component {
     renderItem2 = ({ item }) => {
         return (
             <TouchableOpacity style={{ flexDirection: 'row', marginStart: 10, marginBottom: 10, marginEnd: 10, width: deviceWidth - 20, height: 100, backgroundColor: '#FFFFFF', borderLeftColor: '#ebaa34', borderLeftWidth: 5 }}>
-                <View style={{ width: (deviceWidth - 10) / 4, backgroundColor: 'green' }}>
-
+                <View style={{ width: (deviceWidth - 10) / 4, backgroundColor: '#FFFFFF',alignItems:'center',justifyContent:'center' }}>
+                    <Image source={item.image} style={{ width: 100, height: 100 }} />
                 </View>
-                <View style={{ width: (deviceWidth - 10) / 4 * 3, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>Khuyến mại </Text>
-                    <Text>Khuyến mại Khuyến mại </Text>
+                <View style={{ width: (deviceWidth - 10) / 4 * 3, alignItems: 'center', justifyContent: 'center',
+             }}>
+                    <Text style={{fontWeight:'bold',color:'red'}}>Khuyến mại </Text>
+                    <Text>{item.content}</Text>
                 </View>
             </TouchableOpacity>
         )
@@ -33,14 +31,10 @@ export default class Promotion extends Component {
 
     render() {
         const data1 = [
-            { id: '1', image: vcs },
-            { id: '2', image: nhac },
-            { id: '3', image: sun },
-            { id: '4', image: saigon },
-            { id: '1', image: vcs },
-            { id: '2', image: nhac },
-            { id: '3', image: sun },
-            { id: '4', image: saigon },
+            { id: '1', image: sale, content: "Giảm giá 50% khi mua 4 vé trở lên" },
+            { id: '2', image: sale, content: "Giảm giá 20% từ 22-4 đến 2-5-2019" },
+            { id: '3', image: sale, content: "Miễn phí cho người thứ 5" },
+            { id: '4', image: sale, content: "Miễn phí cho trẻ dưới 6 tuổi" },
         ]
 
         return (
--- src/stories/screens/Recharge/index.tsx
+++ src/stories/screens/Recharge/index.tsx
@@ -1,11 +1,10 @@
 
 import React, { Component } from 'react';
 import { Dimensions, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, TextInput, ScrollView } from 'react-native';
-
-import vcs from '../../../../assets/vcs.png'
-import sun from '../../../../assets/sun.png'
-import nhac from '../../../../assets/nhac.png'
-import saigon from '../../../../assets/saigon.png'
+import { RadioButton } from 'react-native-paper';
+import visaCard from '../../../../assets/Bus2FloorTicket/visaCard.png'
+import masterCard from '../../../../assets/Bus2FloorTicket/masterCard.png'
+import atm from '../../../../assets/Bus2FloorTicket/atm.png'
 const deviceHeight = Dimensions.get("window").height;
 const deviceWidth = Dimensions.get("window").width;
 
@@ -26,7 +25,7 @@ export default class Recharge extends Component {
                     <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#85563a' }}>Số dư trong ví</Text>
                     <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#85563a' }}>69.000 đ</Text>
                 </View>
-                <View style={{ width: deviceWidth, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingStart: 10, paddingEnd: 20, marginTop: 10}}>
+                <View style={{ width: deviceWidth, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingStart: 10, paddingEnd: 20, marginTop: 10 }}>
                     <TextInput
                         placeholder='Nhập số tiền'
                         placeholderTextColor='#999999'
@@ -39,6 +38,30 @@ export default class Recharge extends Component {
                 <View style={{ width: deviceWidth, backgroundColor: '#e0e0e0', height: 40, justifyContent: 'center', marginTop: 10 }}>
                     <Text style={{ fontSize: 16, marginStart: 10 }}>CHỌN NGUỒN TIỀN</Text>
                 </View>
+
+                <RadioButton.Group
+                    value={'first'}
+                    onValueChange={() => { }}>
+                    <View style={{ height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingEnd: 15 }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', marginStart: 10 }}>
+                            <RadioButton value="second" color='#0c7e7c' />
+                            <Text style={{ marginStart: 10, color: '#000000' }}>Thẻ tín dụng/ghi nợ</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Image source={visaCard} style={{ marginEnd: 5 }} />
+                            <Image source={masterCard} />
+                        </View>
+                    </View>
+                    <View style={styles.grayline} />
+                    <View style={{ height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingEnd: 15 }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', marginStart: 10 }}>
+                            <RadioButton value="third" color='#0c7e7c' />
+                            <Text style={{ marginStart: 10, color: '#000000' }}>Thẻ ATM nội địa</Text>
+                        </View>
+                        <Image source={atm} />
+                    </View>
+                    <View style={styles.grayline} />
+                </RadioButton.Group>
                 <TouchableOpacity style={{ position: 'absolute', bottom: 0, width: deviceWidth, height: 45, backgroundColor: '#ebaa34', alignItems: 'center', justifyContent: 'center' }}>
                     <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 16 }}>NẠP TIỀN</Text>
                 </TouchableOpacity>
